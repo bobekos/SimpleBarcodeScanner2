b7117185d634214f10a4e1f89005de9bfb51c7c1
Bobekos <bobek.bobekos@gmail.com>
2019-08-02 22:57:18 +0200

    camera 1 preview

--- simplebarcodescanner2/src/main/java/com/github/bobekos/simplebarcodescanner2/camera/v1/Camera1Preview.kt
+++ simplebarcodescanner2/src/main/java/com/github/bobekos/simplebarcodescanner2/camera/v1/Camera1Preview.kt
@@ -6,26 +6,26 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.OnLifecycleEvent
+import java.lang.Math.abs
 
-class Camera1Preview(val camera: Camera) : LifecycleObserver {
+class Camera1Preview(val camera: Camera, displaySize: Size) : LifecycleObserver {
 
-    private val cameraParameters: Camera.Parameters = camera.parameters
+    private var previewSize: Size
 
     init {
-        //TODO FOCUS MODE only by back
-        cameraParameters.focusMode = Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE
+        val parameters = camera.parameters
+
+        previewSize = getValidPreviewSize(displaySize)
+
+        parameters.setPreviewSize(previewSize.width, previewSize.height)
+        parameters.setRotation(90)
+        parameters.focusMode = Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE
+
+        camera.parameters = parameters
     }
 
-    fun setPreviewSize(previewSize: Size) = apply {
-        cameraParameters.setPreviewSize(previewSize.width, previewSize.height)
-    }
-
-    fun setRotation(rotation: Int) = apply {
-        cameraParameters.setRotation(rotation)
-    }
-
-    fun build() = apply {
-        camera.parameters = cameraParameters
+    fun setOnPreviewOutputListener(block: (camera: Camera, previewSize: Size) -> Unit) {
+        block(camera, previewSize)
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
@@ -40,5 +40,23 @@ class Camera1Preview(val camera: Camera) : LifecycleObserver {
 
     fun bindToLifecycle(lifecycleOwner: LifecycleOwner) {
         lifecycleOwner.lifecycle.addObserver(this)
+    }
+
+    private fun getValidPreviewSize(size: Size): Size {
+        val supportedPreviewSize = camera.parameters.supportedPreviewSizes
+
+        var result = size
+        var minDiff = Int.MAX_VALUE
+
+        supportedPreviewSize.forEach {
+            val diff = abs(it.width - size.width) + abs(it.height - size.height)
+
+            if (diff < minDiff) {
+                result = Size(it.width, it.height)
+                minDiff = diff
+            }
+        }
+
+        return result
     }
 }
